c54f7ccf140e9e18058d5746b109c7f28b32bdfd
sarleon <sarleon56@hotmail.com>
2016-09-26 11:05:17 -0500

    add comment api

--- Music163API.py
+++ Music163API.py
@@ -2,7 +2,7 @@
 import requests
 import re
 from bs4 import BeautifulSoup
-from json import JSONDecoder
+import json
 class Music163Api:
     cookies={'appver':'1.5.0.75771'}
     headers={
@@ -42,11 +42,11 @@ class Music163Api:
 
 
     """
-    获取一个歌手的专辑列表
+    获取一个歌手的信息
     artist_id是歌手的id
 
     """
-    def get_albums_of_artist(self,artist_id):
+    def get_songs_of_artist(self,artist_id):
         cookies={'appver':'1.5.0.75771'}
         headers={
             'Referer': 'http://music.163.com/'
@@ -57,6 +57,36 @@ class Music163Api:
             'offset':'0'
         }
         page=requests.get(url=url,data=data,cookies=self.cookies,headers=self.headers)
-        print page.content
 
-        return  page
+        pattern=re.compile("\"id\":\s*(\d+)")
+        match=re.findall(pattern,page.content)
+        print len(match)
+        for item in match:
+            #print item
+            pass
+        map_data=json.loads(page.content)
+        songs= map_data[u'hotSongs']
+        song_ids=[]
+        for song in songs:
+            song_ids.append(song[u'id'])
+
+        #print page.content
+
+        return  song_ids
+    def get_song_detail_bak(self,song_id):
+        data={
+            'id':song_id,
+            'ids':"["+str(song_id)+"]"
+        }
+        url='http://music.163.com/api/song/detail/'
+        page=requests.post(url=url,data=data,cookies=self.cookies,headers=self.headers)
+
+        print page.content
+    def get_song_detail(self,song_id):
+        data={
+            'id':song_id
+        }
+        url='http://music.163.com/api/playlist/detail?id='+str(song_id)
+        page=requests.get(url=url)
+
+        print page.content
--- test.py
+++ test.py
@@ -1,10 +1,12 @@
 #coding:utf-8
 from Music163API import Music163Api
-def test():
+def test2():
     apis=Music163Api()
-    apis.get_albums_of_artist(3252)
-
+    apis.get_songs_of_artist(1071081)
+def test3():
+    apis=Music163Api()
+    apis.get_song_detail(11362719)
 
 
 if __name__=='__main__':
-   test()
+   test3()
