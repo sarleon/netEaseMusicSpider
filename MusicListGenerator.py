3c9917ae1e14793a13ff65244008611430dbce45
sarleon <sarleon56@hotmail.com>
2016-09-26 11:57:12 -0500

    test json data

--- MusicListGenerator.py
+++ MusicListGenerator.py
@@ -3,19 +3,22 @@ import requests
 from bs4 import BeautifulSoup
 import MySQLdb
 from Config import Config
+from Music163API import Music163Api
 
 
 class MusicListGenerator:
     ARTIST_ID_UP_LIMIT = 1000000
-    ARTIST_BASE_URL = "http://music.163.com/#/artist?id="
+
     def __init__(self):
         db = MySQLdb.connect ( Config.MYSQL_URL , Config.MYSQL_USERNAME , Config.MYSQL_PASSWORD , Config.MYSQL_DATABASE )
         cursor = db.cursor ( )
+        self.api=Music163Api()
 
     def __get_song_list ( self , artist_page_text ):
         pass
 
     def __try_artist_id ( self , artist_id ):
-        current_url = self.ARTIST_BASE_URL + str ( artist_id )
-        page = requests.get ( current_url )
-        print page.text
+        return self.api.get_songs_of_artist(artist_id)
+
+if __name__ == '__main__':
+    mlg=MusicListGenerator()
